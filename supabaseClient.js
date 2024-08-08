557c2ab8699108ebc33e52ba1138e9f2b17aecfe
Don_Aveo <133364365+BrendonAvelino@users.noreply.github.com>
2024-08-08 16:06:49 -0300

    Commit_API

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://ebmrokypzelpuuqttigx.supabase.co';
-const SUPABASE_KEY = /* 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVibXJva3lwemVscHV1cXR0aWd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxMTc0NzEsImV4cCI6MjAzODY5MzQ3MX0.1Xj3bLqmPx_qcfkKix3zdAmAVg7kuo1pPIH1O6scktg'; */
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVibXJva3lwemVscHV1cXR0aWd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxMTc0NzEsImV4cCI6MjAzODY5MzQ3MX0.1Xj3bLqmPx_qcfkKix3zdAmAVg7kuo1pPIH1O6scktg'; 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
   localStorage: AsyncStorage,
